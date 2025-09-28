53d2a1a3764cae536eac2f6f97215edcdb47daca
Steve Banh <steve.banh02@gmail.com>
2025-09-28 18:07:00 -0400

    first commit

---
+++ ExerciseTwo/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class LINQWithArrayOfObjects
+{
+	static void Main()
+	{
+        var employees = new[]
+           {
+                new Employee {"Jason", "Red", 5000M},
+                new Employee {"Ashley", "Green", 7600M},
+                new Employee {"Matthew", "Indigo", 3587.5M},
+
+
+            };
+
+        Console.WriteLine("Original array:");
+        foreach (var element in employees)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
