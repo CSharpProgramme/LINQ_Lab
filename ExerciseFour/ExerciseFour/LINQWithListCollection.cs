52798ad4c05728e7100b2a21c4ef37be6b3b1d4b
Steve Banh <steve.banh02@gmail.com>
2025-09-30 01:59:29 -0400

    lab done need double check

---
+++ ExerciseFour/ExerciseFour/LINQWithListCollection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseFour
+{
+    internal class LINQWithListCollection
+    {
+        static void Main()
+        {
+            //populate a list of strings
+            var items = new List<string>();
+            items.Add("aQua"); //add "aQua" to the end of the list
+            items.Add("RusT"); //add "RusT" to the end of the list
+            items.Add("yElLow"); //add "yElLow" to the end of the list
+            items.Add("rEd"); //add "rEd" to the end of the list
+
+            //display intial list
+            Console.Write("items contains:");
+            foreach (var item in items)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();//output end of line
+
+            //convert to uppercase, select those starting with "R" and sort
+            var startsWithR = from item in items
+                             let uppercaseString = item.ToUpper()
+                             where uppercaseString.StartsWith("R")
+                             orderby uppercaseString
+                             select uppercaseString;
+
+            //display query results
+            Console.Write("results of query startsWithR:");
+            foreach(var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();//output end of line
+
+            items.Add("rUbY");//add "rUbY" to the end of the list
+            items.Add("SaFfRon");//add "SaFfRon" to the end of the list
+
+            //display intial list
+            Console.Write("items contains:");
+            foreach (var item in items)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();//output end of line
+
+            //display query results
+            Console.Write("results of query startsWithR:");
+            foreach (var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();//output end of line
+        }
+    }
+}
---
+++ ExerciseThree/ExerciseThree/ListCollection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseThree
+{
+    internal class ListCollection
+    {
+        static void Main()
+        {
+            //create a new list of strings
+            var items = new List<string>();
+
+            //display list's count and capacity before adding elements
+            Console.WriteLine("Before adding to items: " + $"Count = {items.Count}; Capacity = {items.Capacity}");
+
+            items.Add("red"); //append an item to the list
+            items.Insert(0, "yellow"); //insert the value at index 0
+
+            //display list's count and capacity after adding two elements
+            Console.WriteLine("After adding two elements to items: " + $"Count = {items.Count}; Capacity = {items.Capacity}");
+
+            //display the colors in the list
+            Console.WriteLine("\nDisplay list contents with counter-controlled loop:");
+            for (var i = 0; i < items.Count; i++)
+            {
+                Console.Write($"{items[i]} ");
+            }
+
+            //display the colors using foreach
+            Console.WriteLine("\nDisplay list contents with foreach statement:");
+            foreach(var item in items)
+            {
+                Console.Write($"{item} ");
+            }
+
+            items.Add("green"); //add "green" to the end of the list
+            items.Add("yellow"); //add "yellow" to the end of the list
+
+            //display list's count and capacity after adding two more elements
+            Console.WriteLine("\n\nAfter adding two more elements to items: " + $"Count = {items.Count}; Capacity = {items.Capacity}");
+
+            //display the list
+            Console.Write("\nList with two new elements:");
+            foreach(var item in items)
+            {
+                Console.Write($" {item} ");
+            }
+
+            items.Remove("yellow"); //remove the first "yellow"
+
+            //display the list
+            Console.Write("\n\nremove first instance of yellow:");
+            foreach (var item in items)
+            {
+                Console.Write($" {item} ");
+            }
+
+            items.RemoveAt(1); //remove item at index 1
+
+            //display the list
+            Console.Write("\n\nremove second instance (green):");
+            foreach (var item in items)
+            {
+                Console.Write($" {item} ");
+            }
+
+            //display list's count and capacity after removing two elements
+            Console.WriteLine("\n\nAfter removing two elements from items: " + $"Count = {items.Count}; Capacity = {items.Capacity}");
+
+            //check if a value is in the list
+            Console.WriteLine("\n\"red\" is " + $"{(items.Contains("red") ? string.Empty : "not ")}in the list");
+
+            items.Add("orange"); //add "orange" to the end of the list
+            items.Add("violet"); //add "violet" to the end of the list
+            items.Add("blue"); //add "blue" to the end of the list
+
+            //display list's count and capacity after adding three elements
+            Console.WriteLine("\nAfter adding three more elements to items: " + $"Count = {items.Count}; Capacity = {items.Capacity}");
+
+            //display the list
+            Console.Write("list with three new elements:");
+            foreach (var item in items)
+            {
+                Console.Write($" {item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
